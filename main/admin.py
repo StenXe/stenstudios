dcf576e367a33042ccc013d61d17ade333bcc8c5
StenXe <sarvesh.tendulkar@gmail.com>
2019-05-12 15:37:24 +0530

    Model Changes, TinyMCE added to content.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
+from tinymce.widgets import TinyMCE
+from django.db import models
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+            ("Title & Date", {"fields": ["title", "date"]}),
+            ("Content", {"fields": ["content"]})
+        ]
+
+    formfield_overrides = {
+            models.TextField: {'widget': TinyMCE()}
+        }
+admin.site.register(Post, PostAdmin)
--- main/models.py
+++ main/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    date = models.DateTimeField("Date Published")
+    date = models.DateTimeField("Date Published", default=datetime.now)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name_plural = "Posts"
