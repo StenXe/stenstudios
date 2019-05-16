e862d091058d61ee0a4258ea89ebaeff10d183df
StenXe <sarvesh.tendulkar@gmail.com>
2019-05-16 23:44:54 +0530

    Modified home page, Added Registration page

--- main/views.py
+++ main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 
 def homepage(request):
     return render(request,
                   "main/homepage.html",
                   {"posts": Post.objects.all()})
+
+def register(request):
+    form = UserCreationForm()
+    return render(request,
+                  "main/register.html",
+                  {"form": form})
